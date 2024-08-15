6eed5f31172b31a0d540d3dbdf21c5989ebbff60
Fernando José Flores Mendoza <110640664+ferflores28032002@users.noreply.github.com>
2024-08-15 10:07:03 -0600

    se agrego config de cors

--- src/presentation/Server.ts
+++ src/presentation/Server.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 
 interface Options {
   port?: number;
-  routes: Router
+  routes: Router;
 }
 
 export class Server {
@@ -19,13 +19,22 @@ export class Server {
   }
 
   async start() {
-    //Middelware
+    // Middleware
 
+    // Configuración avanzada de CORS
+    const corsOptions = {
+      origin: '*', // o un dominio específico en lugar de '*'
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+      credentials: true, // Si necesitas manejar cookies o autenticación
+    };
+
+    this.app.use(cors(corsOptions)); // Usa las opciones avanzadas de CORS
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(cors());
 
-    this.app.use(this.routes)
+    // Rutas
+    this.app.use(this.routes);
 
     this.app.listen(this.port, () => {
       console.log(`Server running on port ${this.port} 🚀`);
